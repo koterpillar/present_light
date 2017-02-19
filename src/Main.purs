4c7175bc2ca15a3e5dc2391c4af30b23914d821b
Alexey Kotlyarov <a@koterpillar.com>
2017-02-19 16:28:43 +1100

    React component

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,37 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Data.Map as M
+
+import React as R
+import React.DOM as R
+import React.DOM.Props as RP
+import ReactDOM as RDOM
+
+import Thermite as T
+
+
+data Understanding = TooEasy | Normal | TooHard
+
+type WatcherId = Int
+
+data Action = SetUnderstanding WatcherId Understanding
+
+type State = { understanding :: M.Map WatcherId Understanding }
+
+
+initialState :: State
+initialState = { understanding : M.empty }
+
+render :: T.Render State _ Action
+render dispatch _ state _ =
+  [ R.div' [] ]
+
+performAction :: T.PerformAction _ State _ Action
+performAction (SetUnderstanding watcher value) _ _ = void (T.cotransform (\state -> state { understanding = M.insert watcher value state.understanding }))
+
+spec :: T.Spec _ State _ Action
+spec = T.simpleSpec performAction render
+
+main = T.defaultMain spec initialState unit
