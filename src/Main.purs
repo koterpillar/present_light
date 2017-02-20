6be5b92ebcf049b34fcd1ed52162668cf6a19348
Alexey Kotlyarov <a@koterpillar.com>
2017-02-21 08:05:00 +1100

    Render bar and watcher buttons

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,9 @@ module Main where
 
 import Prelude
 
+import Data.Generic
+import Data.Int
+import Data.List as L
 import Data.Map as M
 
 import React as R
@@ -13,6 +16,10 @@ import Thermite as T
 
 
 data Understanding = TooEasy | Normal | TooHard
+derive instance uGeneric :: Generic Understanding
+
+instance uEq :: Eq Understanding where
+  eq = gEq
 
 type WatcherId = Int
 
@@ -25,8 +32,43 @@ initialState :: State
 initialState = { understanding : M.empty }
 
 render :: T.Render State _ Action
-render dispatch _ state _ =
-  [ R.div' [] ]
+render dispatch _ state _ = [ R.div [ RP.style { width: "400px", height: "100px" } ]
+                              (map (renderUnderstanding state) [TooEasy, Normal, TooHard])
+                            , renderButtons dispatch 1
+                            , renderButtons dispatch 2
+                            , renderButtons dispatch 3
+                            , renderButtons dispatch 4
+                            ]
+
+cssPercentage :: Number -> String
+cssPercentage value = show (value * 100.0) <> "%"
+
+understandingPercentage :: State -> Understanding -> Number
+understandingPercentage state u = (toNumber matching) / toNumber (M.size state.understanding)
+  where matching = L.length (L.filter (eq u) (M.values state.understanding))
+
+understandingColor :: Understanding -> String
+understandingColor TooEasy = "#00ff00"
+understandingColor Normal = "#ffff00"
+understandingColor TooHard = "#ff0000"
+
+renderUnderstanding :: State -> Understanding -> R.ReactElement
+renderUnderstanding state u = R.div [ RP.style { width: cssPercentage (understandingPercentage state u)
+                                               , height: "100px"
+                                               , backgroundColor: understandingColor u
+                                               , display: "inline-block"
+                                               }
+                                    ] []
+
+renderButtons :: (Action -> T.EventHandler) -> WatcherId -> R.ReactElement
+renderButtons dispatch watcher = R.p' [ R.button [ RP.onClick \_ -> dispatch (SetUnderstanding watcher TooEasy)]
+                                        [ R.text "Easy"]
+                                      , R.button [ RP.onClick \_ -> dispatch (SetUnderstanding watcher Normal)]
+                                        [ R.text "Normal"]
+                                      , R.button [ RP.onClick \_ -> dispatch (SetUnderstanding watcher TooHard)]
+                                        [ R.text "Hard"]
+
+                                      ]
 
 performAction :: T.PerformAction _ State _ Action
 performAction (SetUnderstanding watcher value) _ _ = void (T.cotransform (\state -> state { understanding = M.insert watcher value state.understanding }))
